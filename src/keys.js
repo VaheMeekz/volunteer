3b1c6913a25836fe3c60afd820c3b422072827c8
Vahe <71688163+VaheMeekz@users.noreply.github.com>
2022-06-21 18:32:45 +0400

    config

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,4 @@
 export const keys = {
-  // API_URI: "https://volunteer002.herokuapp.com",
-  API_URI: "http://localhost:5000",
+  API_URI: "https://volunteer002.herokuapp.com",
+  // API_URI: "http://localhost:5000",
 };
